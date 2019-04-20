f950ec081244474a844699d7053073b5bf1124ee
Island-c <liu0148@outlook.com>
2019-04-20 20:23:58 +0800

    修改了数据导出地址

--- src/Partitioning/Config.java
+++ src/Partitioning/Config.java
@@ -26,19 +26,20 @@ public class Config {
 	static public List<HashSet<Node>> clusters_node=new ArrayList<HashSet<Node>>();//用于存储每个cluster中的不同Node
 	
 	////地址
-	public static String root_path="C://Users//liu01//Desktop//red_building//"; //根目录
+	public static String root_path=".//Database//"; //根目录
 	//原始数据读取地址
 	public static String read_csv_filePath=root_path+"Database_"+database+"//"+database+".csv";//原始数据csv文件地址
-	//日志保存地址
-	public static String spo_store_filePath=root_path+"Database_"+database+"//Log//spo_dataset.txt";//spo三元组存储地址	
-	public static String hashmap_store_filePath=root_path+"Database_"+database+"//Log";//节点密度数据存储地址
-	public static String cliquelog_filePath=root_path+"Database_"+database+"//Log//cliqueLog.txt"; //clique聚类日志
-	public static String ClusterCsvFilePath=root_path+"Database_"+database+"//Log//cluster.csv";//集群信息
+
 	//信息保存地址
+	public static String spo_store_filePath=root_path+"Database_"+database+"//Data//spo_dataset.txt";//spo三元组存储地址	
+	public static String hashmap_store_filePath=root_path+"Database_"+database+"//Data";//节点密度数据存储地址
+	public static String ClusterCsvFilePath=root_path+"Database_"+database+"//Data//cluster.csv";//集群信息
 	public static String NodecsvFilePath=root_path+"Database_"+database+"//Data//node.csv";//节点信息
 	public static String Index_tablePath=root_path+"Database_"+database+"//Cluster//Index_table.txt";//索引表
 	public static String ClusterPath=root_path+"Database_"+database+"//Cluster//"; //写不同集群的三元组
-	
+	//日志保存地址
+	public static String cliquelog_filePath=root_path+"Database_"+database+"//Log//cliqueLog.txt"; //clique聚类日志
+
 
 
 }
